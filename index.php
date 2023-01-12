474ec245cbf7da080c42578529cd39d649a5e563
Ericdte <96052582+Ericdte@users.noreply.github.com>
2023-01-12 02:14:27 +0100

    Update index.php
    djou

--- index.php
+++ index.php
@@ -1,112 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>Kam et fils </title>
-    <!-- MDB icon -->
-    <link rel="icon" href="img/kams.jpg" type="image/x-icon" />
-    <!-- Font Awesome -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-    />
-    <!-- Google Fonts Roboto -->
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
-    />
-    <!-- MDB -->
-    <link rel="stylesheet" href="css/mdb.min.css" />
-  </head>
-  <body>
-    <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <!-- Container wrapper -->
-  <div class="container-fluid">
-    <!-- Toggle button -->
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-mdb-toggle="collapse"
-      data-mdb-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
 
-    <!-- Collapsible wrapper -->
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!-- Navbar brand -->
-      <a class="navbar-brand mt-2 mt-lg-0" href="#">
-        <img
-        src ="img/kams.jpg" 
-          height="50"
-          width="50"
-          alt="kam et fils logo"
-          loading="lazy"
-        />
-      </a>
-      <!-- Left links -->
-      <div class="dropdown">
-        <button
-          class="btn btn-primary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton"
-          data-mdb-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Departments
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <li><a class="dropdown-item" href="#">Marketing</a></li>
-          <li><a class="dropdown-item" href="#">finance and administration</a></li>
-          <li><a class="dropdown-item" href="#">Quarries </a></li>
-          <li><a class="dropdown-item" href="#">road and bridge construction</a></li>
-          <li><a class="dropdown-item" href="#">transport</a></li>
-          <li><a class="dropdown-item" href="#">building and construction</a></li>
-          <li><a class="dropdown-item" href="#">technology</a></li>
-          <li><a class="dropdown-item" href="#">machinery</a></li>
-          <li><a class="dropdown-item" href="#">field engineering</a></li>
-          <li><a class="dropdown-item" href="#">electrical</a></li>
-          
-        </ul>
-      </div>
-      <!-- Left links -->
-    </div>
-    <!-- Collapsible wrapper -->
-</nav>
-<!-- Background image -->
-<div
-class="p-5 text-center bg-image"
-style="
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+	<title>Kam et fils </title>
+	<!-- MDB icon -->
+	<link rel="icon" href="img/kams.jpg" type="image/x-icon" />
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+	<!-- Google Fonts Roboto -->
+	<link rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+	<!-- MDB -->
+	<link rel="stylesheet" href="css/mdb.min.css" />
+</head>
+
+<body>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<!-- Container wrapper -->
+		<div class="container-fluid">
+			<!-- Toggle button -->
+			<button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+				data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+				aria-label="Toggle navigation">
+				<i class="fas fa-bars"></i>
+			</button>
+
+			<!-- Collapsible wrapper -->
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<!-- Navbar brand -->
+				<a class="navbar-brand mt-2 mt-lg-0" href="#">
+					<img src="img/kams.jpg" height="50" width="50" alt="kam et fils logo" loading="lazy" />
+				</a>
+				<!-- Left links -->
+				<div class="dropdown">
+					<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton"
+						data-mdb-toggle="dropdown" aria-expanded="false">
+						Departments
+					</button>
+					<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+						<li><a class="dropdown-item" href="#">Marketing</a></li>
+						<li><a class="dropdown-item" href="#">finance and administration</a></li>
+						<li><a class="dropdown-item" href="#">Quarries </a></li>
+						<li><a class="dropdown-item" href="#">road and bridge construction</a></li>
+						<li><a class="dropdown-item" href="#">transport</a></li>
+						<li><a class="dropdown-item" href="#">building and construction</a></li>
+						<li><a class="dropdown-item" href="#">technology</a></li>
+						<li><a class="dropdown-item" href="#">machinery</a></li>
+						<li><a class="dropdown-item" href="#">field engineering</a></li>
+						<li><a class="dropdown-item" href="#">electrical</a></li>
+
+					</ul>
+				</div>
+				<!-- Left links -->
+			</div>
+			<!-- Collapsible wrapper -->
+	</nav>
+	<!-- Background image -->
+	<div class="p-5 text-center bg-image" style="
 
   height: 400px;
   margin-top: 58px;
-"
->
-<div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
-  <div class="d-flex justify-content-center align-items-center h-100">
-    <div class="text-white">
-      <h1 class="mb-3">KAM ET FILS</h1>
-      <h4 class="mb-3">Welcome to kam et fils company.<br>Here we deal we road building ,
-        digging and construction,<br>we also offer trucks rental service.<br>
-      your satisfaction is our priority</h4>
-    </div>
-  </div>
-</div>
-</div>
-    <div class="container">
-     
-    </div>
-    <!-- End your project here-->
-    <!-- MDB -->
-<script type="text/javascript" src="js/mdb.min.js"></script>
-<!-- Custom scripts -->
-<script type="text/javascript"></script>
-</nav>
+">
+		<div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+			<div class="d-flex justify-content-center align-items-center h-100">
+				<div class="text-white">
+					<h1 class="mb-3">Djou Tousse Eric</h1>
+					<h4 class="mb-3">Welcome to kam et fils company.<br>Here we deal we road building ,
+						digging and construction,<br>we also offer trucks rental service.<br>
+						your satisfaction is our priority</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+
+	</div>
+	<!-- End your project here-->
+	<!-- MDB -->
+	<script type="text/javascript" src="js/mdb.min.js"></script>
+	<!-- Custom scripts -->
+	<script type="text/javascript"></script>
+	</nav>
 </body>
+
 </html>
